72a6312b2062351759b3efda40b60aa8b55db5a3
shaco-star <sherifm846@gmail.com>
2023-05-14 08:25:11 -0400

    commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     if idx > (len(my_list) - 1) or idx < 0:
         return my_list
     my_list[idx] = element
-    return my_list
+    return my_list 
