834841fc8b3f5d2b6fe33dd89927a728d145b790
shaco-star <sherifm846@gmail.com>
2023-05-14 08:23:07 -0400

    commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,5 @@
 def element_at(my_list, idx, element):
     if idx > (len(my_list) - 1) or idx < 0:
         return my_list
-    return my_list[idx] = element
+    my_list[idx] = element
+    return my_list
