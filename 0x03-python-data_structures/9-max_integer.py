43b6509e66d385c64b2df38622283363507024d4
shaco-star <sherifm846@gmail.com>
2023-05-15 04:03:15 -0400

    commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = 0
+    for x in my_list:
+        if max < x:
+            max = x
+    return max        
