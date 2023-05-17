3c66be22a80bf11bef967cf709220e1419976869
shaco-star <sherifm846@gmail.com>
2023-05-17 15:45:30 -0400

    commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     new_list = []
     sum = 0
     for i in my_list:
-        for i not in new_list:
+        if i not in new_list:
             sum += i
             new_list.append(i)
     return sum
