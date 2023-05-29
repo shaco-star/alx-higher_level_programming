5204526c208a430f6647440104bf4619a47467e3
shaco-star <sherifm846@gmail.com>
2023-05-29 06:49:04 -0400

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if (int(value)):
-            print("{:d}".format(value))
+        print("{:d}".format(value))
     except ValueError:
         return False
     else:
