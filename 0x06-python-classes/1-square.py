e11f70140a718fb58a1a3580ed526334ef712adb
shaco-star <sherifm846@gmail.com>
2023-05-30 13:49:48 -0400

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+"""Define Square class"""
+
+
 class Square:
+    """Represent a square"""
+
     def __init__(self, size):
+        """Initialize a new square
+        Args:
+        size (int) : size of the new sqaure
+        """
         self.__size = size
