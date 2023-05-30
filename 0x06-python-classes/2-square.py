73cea4869a99f09556ff85226173c62a7622fda7
shaco-star <sherifm846@gmail.com>
2023-05-30 05:56:59 -0400

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+
 """Define class Square"""
 
 
 class Square:
     def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+            size (int): The size of new square
+        """    
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
