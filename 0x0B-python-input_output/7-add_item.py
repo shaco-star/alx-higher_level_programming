85d0aa52d0205f38b41c1d075966983f6dddd5be
shaco-star <sherifm846@gmail.com>
2023-06-13 15:32:12 -0400

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@
 
 import json
 import sys
-from os import path
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
