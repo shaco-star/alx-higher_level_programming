fb6c87ee69e73fdaa04486f3e2b25ab965ea4022
shaco-star <sherifm846@gmail.com>
2023-06-13 15:30:10 -0400

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,12 +13,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if path.isfile(filename):
+if path.exists(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
-
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
+my_list.extend(sys.argv[1:])
 save_to_json_file(my_list, filename)
