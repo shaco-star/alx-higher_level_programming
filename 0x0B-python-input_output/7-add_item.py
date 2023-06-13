d7e694727058ea17349fc638ae35a25b43f7106d
shaco-star <sherifm846@gmail.com>
2023-06-13 15:05:49 -0400

    commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""sys module to handle args passed
+    path to locate files on system
+"""
+
+import sys
+from os import path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_to_json_file(my_list, filename)
