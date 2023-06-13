4f4d4ce808c4087628adbdacc9219c8bfbc47c44
shaco-star <sherifm846@gmail.com>
2023-06-13 14:24:58 -0400

    commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Define read_file function that read file"""
+
+
+def read_file(filename="", text=""):
+    """print the content of file"""
+    with open(filename, 'w', encoding='uft-8') as f:
+        return f.write(text)
