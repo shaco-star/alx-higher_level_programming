0e2f539b0b6c2f3066b04a85e3378a6efd6a0c4a
shaco-star <sherifm846@gmail.com>
2023-06-14 13:26:28 -0400

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@
 
 class MyList(list):
     """A custom list class that inherits from the built-in list class."""
+    def __init__(self):
+        """Initialize Object"""
+        super().__init__()
 
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
