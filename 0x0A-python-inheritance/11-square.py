57dde1e14c86e97d9260f11670173a48a283e442
shaco-star <sherifm846@gmail.com>
2023-06-14 13:46:14 -0400

    fix

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""define class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    def __init__(self, size):
+        """init
+        args:
+            size : size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return area of Square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string represent sqaure"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
