5ad88b4b4349e782b73ff529397b82684b424405
shaco-star <sherifm846@gmail.com>
2023-06-20 12:15:05 -0400

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,10 +23,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Convert dictionary to json format
         """
-        if list_dictionaries is None or list_dictionaries == "[]":
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
