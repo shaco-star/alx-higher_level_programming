d57ba80d155f3b8095493fb95619a35ac6fa1e52
shaco-star <sherifm846@gmail.com>
2023-06-20 12:24:06 -0400

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,25 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Convert from json to string
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        write json converted string to file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w', encoding='utf-8') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                dic = [x.to_dictionary() for x in list_objs]
+                file.write(Base.to_json_string(dic))
