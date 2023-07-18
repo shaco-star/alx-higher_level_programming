2f7719f34f8cd760395bc4d78cc7de677f9e13c4
shaco-star <sherifm846@gmail.com>
2023-07-18 05:39:50 -0400

    fix-style

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
---creates a table called first_table in the current database in your MySQL server.
+-- creates a table called first_table in the current database in your MySQL server.
 
 create table if not exists first_table(id INT, name VARCHAR(256));
