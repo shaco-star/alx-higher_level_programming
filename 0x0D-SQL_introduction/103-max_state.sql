88008b709ee2101fb6267e088547b7602aecf854
shaco-star <sherifm846@gmail.com>
2023-07-18 07:09:47 -0400

    advanced

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- displays the max temperature of each state (ordered by State name).
 
-SELECT state, MAX(value) as max_temp from temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
