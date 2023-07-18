15341b6b336e01b1bfb1f860b5b560fdbbc4a566
shaco-star <sherifm846@gmail.com>
2023-07-18 05:31:50 -0400

    first-commit

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+--creates the database hbtn_0c_0 in your MySQL server.
+
+create database if not exists hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+--Delete the database hbtn_0c_0 in your MySQL server.
+
+drop database if exists hbtn_0c_0;
