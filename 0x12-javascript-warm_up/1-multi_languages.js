c1946bc8a55c496abc8be6538099f261830a86c7
shaco-star <sherifm846@gmail.com>
2023-08-14 06:12:12 -0400

    commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing\n';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
