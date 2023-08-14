4dc3d64a2a842d932051986315d833d73e5ac8cd
shaco-star <sherifm846@gmail.com>
2023-08-14 05:58:12 -0400

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-
 import { argv } from 'node:process';
 
-
-let arg = "Argument found";
-if (argv[2] === undefined) arg = "No argument"
-console.log(arg)
+let arg = 'Argument found';
+if (argv[2] === undefined) arg = 'No argument';
+console.log(arg);
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-
 import { argv } from 'node:process';
 
-
 let arg = argv[2];
-if (argv[2] === undefined) arg = "No argument"
-console.log(arg)
+if (argv[2] === undefined) arg = 'No argument';
+console.log(arg);
