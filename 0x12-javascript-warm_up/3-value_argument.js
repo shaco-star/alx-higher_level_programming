9195732c9aa5f3cf9bfa73652dc0df304b0ab9d3
shaco-star <sherifm846@gmail.com>
2023-08-14 06:17:18 -0400

    commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
-
 let arg = argv[2];
-if (argv[2] === undefined) arg = 'No argument';
+if (process.argv[2] === undefined) arg = 'No argument';
 console.log(arg);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
-
-const number = argv[2];
+const number = process.argv[2];
 if (isNaN(number)) { console.log('Not a number'); } else { console.log(`My number: ${number}`); }
