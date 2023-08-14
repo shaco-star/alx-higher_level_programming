b543c46a3b745b83ccc7f862df5bbc363e5c3d05
shaco-star <sherifm846@gmail.com>
2023-08-14 06:16:07 -0400

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
-
 let arg = 'Argument found';
-if (argv[2] === undefined) arg = 'No argument';
+if (process.argv[2] === undefined) arg = 'No argument';
 console.log(arg);
