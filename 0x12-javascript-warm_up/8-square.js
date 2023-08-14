9b316d900f4cb42982617188a99750847f6ed00b
shaco-star <sherifm846@gmail.com>
2023-08-14 06:31:06 -0400

    commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const size = process.argv[2];
-if (isNaN(size)) console.log('Missing size');
-for (let i = 0; i < size; i++) {
-  let row = '';
-  for (let j = 0; j < size; j++) {
-    row += 'x';
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const pstr = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(pstr);
   }
-  console.log(row);
 }
