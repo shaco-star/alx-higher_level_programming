f0929b7ddd9a4e7e54ef49a268a69818b6c08a6e
shaco-star <sherifm846@gmail.com>
2023-08-15 05:43:50 -0400

    commit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,12 +5,12 @@
 const SquareParent = require('./5-square.js');
 
 module.exports = class Square extends SquareParent {
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            console.log(c.repeat(this.width));
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
-}
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
