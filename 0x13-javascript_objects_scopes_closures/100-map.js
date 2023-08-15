9a627c4309ce1eb576f770debee263884d026215
shaco-star <sherifm846@gmail.com>
2023-08-15 05:25:34 -0400

    commit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,4 +3,4 @@
 const importedList = require('./100-data.js').list;
 
 console.log(importedList);
-console.log(importedList.map((ele, index) => ele * index);
+console.log(importedList.map((ele, index) => ele * index));
