6cdfb652fac6125a3729aaaa9a970a11add7f5e7
shaco-star <sherifm846@gmail.com>
2023-08-19 13:18:05 -0400

    commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Print database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access the database and print states
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
