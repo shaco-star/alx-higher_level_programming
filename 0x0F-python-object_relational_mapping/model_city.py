6caae55d6ae5d88b80b0af45f533a9f733c39fd5
shaco-star <sherifm846@gmail.com>
2023-08-20 06:37:35 -0400

    commit

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+"""
+connecting to SQL using ORM
+"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    Creating class State that will connect to state table
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
