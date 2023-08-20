4e7053b93fa1aef4e9638cf93adb6c6330767db1
shaco-star <sherifm846@gmail.com>
2023-08-20 06:06:20 -0400

    commit

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+query State table using ORM
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """using ORM to query State table in MYSQL
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == '2').first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
