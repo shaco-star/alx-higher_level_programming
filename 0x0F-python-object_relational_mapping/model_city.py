2f2aeb97fd3b5976469166bff866d2ec22f6b003
shaco-star <sherifm846@gmail.com>
2023-08-20 06:39:12 -0400

    commit

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -13,6 +13,7 @@ class City(Base):
     Creating class State that will connect to state table
     """
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
