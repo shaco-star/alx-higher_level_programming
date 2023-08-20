1da31246df2321f095e3599909c98a54a57a1204
shaco-star <sherifm846@gmail.com>
2023-08-20 06:22:12 -0400

    commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,9 +12,9 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     """using ORM to query State table in MYSQL
     """
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
