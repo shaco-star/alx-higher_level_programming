57fd146c97c6fef5940555af18525533d9d89568
shaco-star <sherifm846@gmail.com>
2023-09-01 16:31:18 -0400

    commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
 
     repo_name = sys.argv[1]
     user_name = sys.argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'
-    .format(user_name, repo_name)
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(user_name,
+                                                              repo_name)
     response = requests.get(url)
     if response.status_code == 200:
         commits = response.json()[:10]
