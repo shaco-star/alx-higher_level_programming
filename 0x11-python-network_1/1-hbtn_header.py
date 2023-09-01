aa55243f05acd098d99d948a0d255c3c82603a27
shaco-star <sherifm846@gmail.com>
2023-09-01 11:02:24 -0400

    commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-"""sends a request to the URL and displays the value of the X-Request-Id"""
-
-import urllib.request as request
-import sys
-
-url = sys.argv[1]
-with request.urlopen(url) as response:
-    print(response.headers.get('X-Request-Id'))
+"""
+sends a request to the URL and displays the value of the X-Request-Id
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    import sys
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
