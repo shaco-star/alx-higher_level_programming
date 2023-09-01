3b8a09fc85f51840b44854486625c974d2ebcb56
shaco-star <sherifm846@gmail.com>
2023-09-01 16:06:00 -0400

    commit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    r = requests.get(url, data={'email': email})
+    r = requests.post(url, data={'email': email})
     print(r.text)
---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.content)
